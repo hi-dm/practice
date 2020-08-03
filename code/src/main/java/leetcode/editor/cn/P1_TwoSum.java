8c2f08057b3894e6fee1ef85da55a5a738a7709c
madanmei <ma_danmei@163.com>
2020-08-03 22:16:02 +0800

    p1 我们很容易想到用map，然后循环两次 ,却很少想起 一次就行

---
+++ code/src/main/java/leetcode/editor/cn/P1_TwoSum.java
@@ -0,0 +1,52 @@
+//第1题
+//https://leetcode-cn.com/problems/two-sum
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。 
+//
+// 
+//
+// 示例: 
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+// 
+// Related Topics 数组 哈希表 
+// 👍 8807 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//java:两数之和
+public class P1_TwoSum{
+    public static void main(String[] args){
+        Solution solution = new P1_TwoSum().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums==null){
+            return null;
+        }
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int value=target-nums[i];
+            if (map.containsKey(value)){
+                return new int[]{map.get(value),i};
+            }else {
+                map.put(nums[i],i);
+            }
+        }
+        return null;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+ 
