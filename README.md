ceed91c616ef81160fb1efb1646343c10ec99479
madanmei <ma_danmei@163.com>
2020-08-04 22:43:52 +0800

    p143 组合的链表方法 需要多练习

--- README.md
+++ README.md
@@ -16,9 +16,10 @@
 | 待办| 题号 | 题目 | 难度 |  date1 | date2 | 笔记 | 
 | --- | --- | --- | --- | --- | --- |--- |
 | ✔️️| 206 | [reverse-linked-list](https://leetcode.com/problems/reverse-linked-list/) | easy|2020-06-25
-| | 24|[ swap-nodes-in-pairs](https://leetcode.com/problems/swap-nodes-in-pairs)  |medium |2020-08-03|找到遍历的元素 找到头，判断当前的下一个 和下下一个
-| | 141|[ linked-list-cycle]( https://leetcode.com/problems/linked-list-cycle)  |easy|2020-08-04|快慢指针，fast=fast.next.next 
+| ✔️️| 24|[ swap-nodes-in-pairs](https://leetcode.com/problems/swap-nodes-in-pairs)  |medium |2020-08-03|找到遍历的元素 找到头，判断当前的下一个 和下下一个
+| ✔️️| 141|[ linked-list-cycle]( https://leetcode.com/problems/linked-list-cycle)  |easy|2020-08-04|快慢指针，fast=fast.next.next 
 | | 142|[ linked-list-cycle-ii]( https://leetcode.com/problems/linked-list-cycle-ii)  |medium
+| ✔️️| 143|[ linked-list-cycle-ii]( https://leetcode.com/problems/reorder-list)  |medium|2020-08-04|需要多练 组合了链表几个基本方法
 | | 25|[ reverse-nodes-in-k-group]( https://leetcode.com/problems/reverse-nodes-in-k-group/)  |hard
  
 课后作业
---
+++ code/src/main/java/leetcode/editor/cn/P143_ReorderList.java
@@ -0,0 +1,86 @@
+//第143题
+//https://leetcode-cn.com/problems/reorder-list
+//给定一个单链表 L：L0→L1→…→Ln-1→Ln ， 
+//将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→… 
+//
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 示例 1: 
+//
+// 给定链表 1->2->3->4, 重新排列为 1->4->2->3. 
+//
+// 示例 2: 
+//
+// 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3. 
+// Related Topics 链表 
+// 👍 268 👎 0
+
+package leetcode.editor.cn;
+//java:重排链表
+public class P143_ReorderList{
+    public static void main(String[] args){
+        Solution solution = new P143_ReorderList().new Solution();
+    }
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+class Solution {
+    public void reorderList(ListNode head) {
+        if(head==null){
+            return;
+        }
+
+        ListNode mid=findMid(head);
+        ListNode right=mid.next;
+        ListNode left=head;
+
+        mid.next=null;
+        right=revers(right);
+        merge(left,right);
+    }
+
+    private void merge(ListNode left, ListNode right) {
+       while (right!=null){
+        ListNode tmp=right.next;
+        right.next=left.next;
+        left.next=right;
+        left=right.next;
+        right=tmp;
+       }
+    }
+
+    private ListNode revers(ListNode right) {
+        ListNode pre=null;
+        ListNode current=right;
+
+        while (current!=null){
+            ListNode tmp=current.next;
+            current.next=pre;
+            pre=current;
+            current=tmp;
+        }
+        return pre;
+    }
+
+    private ListNode findMid(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while (fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+ 
