f21e2757d1e3699e82fdb86ef59598ce1845a2c5
madanmei <ma_danmei@163.com>
2020-08-13 22:03:51 +0800

    Q21 before after;优化点 ：条件判断多余，公共代码提取，三目运算，额外的变量

--- code/src/main/java/leetcode/editor/cn/Q21_MergeTwoSortedLists.java
+++ code/src/main/java/leetcode/editor/cn/Q21_MergeTwoSortedLists.java
@@ -40,33 +40,20 @@ public class Q21_MergeTwoSortedLists {
 
     class Solution {
         public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-            if (l1 == null) {
-                return l2;
-            }
-            if (l2 == null) {
-                return l1;
-            }
             ListNode pre = new ListNode(0);
             ListNode dummy = pre;
 
             while (l1 != null && l2 != null) {
                 if (l1.val <= l2.val) {
                     pre.next = l1;
-                    pre=l1;
                     l1 = l1.next;
-
                 } else {
                     pre.next = l2;
-                    pre=l2;
                     l2 = l2.next;
                 }
+                pre=pre.next;
             }
-
-            if (l1==null){
-                pre.next=l2;
-            }else {
-                pre.next=l1;
-            }
+            pre.next=l1==null?l2:l1;
             return dummy.next;
         }
     }
