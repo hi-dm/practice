37aabed1629b1a1d239355df0bbb8b8e2e05f187
madanmei <ma_danmei@163.com>
2020-08-27 21:22:13 +0800

    88 diff 答案用了int[--] ；以及System.arraycopy

--- code/src/main/java/leetcode/editor/cn/Q88_MergeSortedArray.java
+++ code/src/main/java/leetcode/editor/cn/Q88_MergeSortedArray.java
@@ -36,27 +36,16 @@ public class Q88_MergeSortedArray {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
-            if (n==0){
+            if (n == 0) {
                 return;
             }
             int p1 = m - 1;
             int p2 = n - 1;
 
             while (p1 >= 0 && p2 >= 0) {
-                if (nums2[p2] > nums1[p1]) {
-                    nums1[p1 + p2 + 1] = nums2[p2];
-                    p2--;
-                } else {
-                    nums1[p1 + p2 + 1] = nums1[p1];
-                    p1--;
-                }
+                nums1[p1 + p2 + 1] = nums2[p2] > nums1[p1] ? nums2[p2--] : nums1[p1--];
             }
-
-            if (p1 < 0) {
-                for (int i = 0; i <= p2; i++) {
-                    nums1[i] = nums2[i];
-                }
-            }
+            System.arraycopy(nums2,0,nums1,0,p2+1);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
