6fde708cea365c842b3a4d344218f85186f95205
madanmei <ma_danmei@163.com>
2020-09-07 20:34:03 +0800

    Q66 可以做个对比 答案更简洁

--- code/src/main/java/leetcode/editor/cn/Q66_PlusOne.java
+++ code/src/main/java/leetcode/editor/cn/Q66_PlusOne.java
@@ -33,16 +33,22 @@ public class Q66_PlusOne {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] plusOne(int[] digits) {
-            for (int i = digits.length - 1; i >= 0; i--) {
-                if (digits[i] + 1 == 10) {
-                    digits[i] = 0;
-                } else {
-                    digits[i] = digits[i] + 1;
-                    return digits;
+            if(null ==digits || digits.length==0){
+                return digits;
+            }
+
+            for (int i = digits.length-1; i >=0; i--) {
+                if (digits[i]==9){
+                    digits[i]=0;
+                }else {
+                    digits[i]=digits[i]+1;
+                    break;
                 }
             }
-            digits = new int[digits.length + 1];
-            digits[0] = 1;
+            if (digits.length>0 && digits[0]==0){
+                digits=new int[digits.length+1];
+                digits[0]=1;
+            }
             return digits;
         }
     }
