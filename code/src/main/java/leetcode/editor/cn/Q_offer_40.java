afaeb7310168071be4218dd667b931095153fc70
madanmei <ma_danmei@163.com>
2020-09-15 22:49:51 +0800

    40 topK PriorityQueue

---
+++ code/src/main/java/leetcode/editor/cn/Q_offer_40.java
@@ -0,0 +1,31 @@
+package leetcode.editor.cn;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Q_offer_40 {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        if(arr.length==0 || k<=0){
+            return new int[0];
+        }
+        Queue<Integer> pq=new PriorityQueue<>((v1,v2)->v2-v1);
+        for (int i = 0; i < arr.length; i++) {
+            if (pq.size()<k){
+                pq.offer(arr[i]);
+            }else {
+                if (arr[i]<pq.peek()){
+                    pq.poll();
+                    pq.offer(arr[i]);
+                }
+            }
+        }
+
+        int[] result=new int[k];
+        int i=0;
+       for (Integer p:pq){
+           result[i]=p;
+           i++;
+       }
+       return result;
+    }
+}
