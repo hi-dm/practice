6a5d877ea780ce160030b6605885d1ddf862569e
madanmei <ma_danmei@163.com>
2020-09-25 21:34:16 +0800

    344 easy

---
+++ code/src/main/java/leetcode/editor/cn/Q344_ReverseString.java
@@ -0,0 +1,51 @@
+//第344题
+//https://leetcode-cn.com/problems/reverse-string
+//编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。 
+//
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。 
+//
+// 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：["h","e","l","l","o"]
+//输出：["o","l","l","e","h"]
+// 
+//
+// 示例 2： 
+//
+// 输入：["H","a","n","n","a","h"]
+//输出：["h","a","n","n","a","H"] 
+// Related Topics 双指针 字符串 
+// 👍 283 👎 0
+
+package leetcode.editor.cn;
+//java:反转字符串
+public class Q344_ReverseString{
+    public static void main(String[] args){
+        Solution solution = new Q344_ReverseString().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void reverseString(char[] s) {
+        if (s==null || s.length<=1){
+            return;
+        }
+
+        int left=0;
+        int right=s.length-1;
+        while (left<right){
+            char tmp=s[left];
+            s[left]=s[right];
+            s[right]=tmp;
+            left++;
+            right--;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+ 
